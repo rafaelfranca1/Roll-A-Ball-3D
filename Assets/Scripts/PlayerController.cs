3e310765c172c824ec9a0ec7085d6f17b3f8f9b6
rafaelfranca1 <rafaeldefranca750@gmail.com>
2025-03-06 19:48:00 -0300

    Add TextMesh Pro assets including emojis, fonts, and shaders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public int speed = 0;
+    public TextMeshProUGUI countText;
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
     void OnMove(InputValue value)
@@ -21,6 +26,11 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 
     void FixedUpdate()
@@ -32,7 +42,11 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("PickUp"))
+        if(other.gameObject.CompareTag("PickUp")){
             other.gameObject.SetActive(false);
+            count++;
+
+            SetCountText();
+        }
     }
 }
